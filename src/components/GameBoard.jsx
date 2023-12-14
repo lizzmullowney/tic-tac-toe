95c1c553962cc0ac874fcf57f12da21352a08c78
Lizz Mullowney <elizabethmullowney@gmail.com>
2023-12-14 15:24:50 -0600

    gameBoard functionality added

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -23,11 +23,11 @@ export default function GameBoard() {
   return (
     <>
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => <li key={rowIndex}>
+      {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
         <ol>
           {row.map((playerSymbol, colIndex) =>
           <li key={colIndex}>
-            <button>{playerSymbol}</button>
+            <button onClick={()=>handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
           </li>)}
         </ol>
       </li>)}
