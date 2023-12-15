9a577edf855fc2238f7dae19ec5771047de22815
Lizz Mullowney <elizabethmullowney@gmail.com>
2023-12-15 12:51:51 -0600

    created functionality for alternating players

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -7,17 +7,18 @@ const initialGameBoard = [
 ]
 
 
-export default function GameBoard() {
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
 
   const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
   function handleSelectSquare(rowIndex, colIndex, ) {
     setGameBoard((previousGameBoard) => {
       const updatedBoard = [...previousGameBoard.map(innerArray =>[...innerArray])];
-      updatedBoard[rowIndex][colIndex] = 'X';
+      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
       return updatedBoard;
 
     });
+    onSelectSquare();
   }
 
   return (
